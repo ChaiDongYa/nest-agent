e4affde6b0befc97b2d462f944064b062ed7694f
柴东 <dong.chai@zhongheschool.com>
2026-05-16 16:24:15 +0800

    feat: 增加post

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DemoModule } from './deme/demo.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [DemoModule, PrismaModule, UserModule, PostModule],
+  imports: [PrismaModule, UserModule, PostModule],
   controllers: [AppController],
   providers: [AppService],
 })
